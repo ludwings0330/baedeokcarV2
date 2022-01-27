98befc426ec92a8a3e16f1b9359aea1cee88679f
ludwings <ludwings0330@gmail.com>
2022-01-27 22:21:01 +0900

    config: h2 database 설정 변경
    - local database -> in memory

---
+++ src/test/java/com/ludwings/baedeokcarv2/domain/model/MemberTest.java
@@ -0,0 +1,25 @@
+package com.ludwings.baedeokcarv2.domain.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class MemberTest {
+
+    @DisplayName("테스트가 수행된다.")
+    @Test
+    public void 테스트() throws Exception {
+        //given
+
+        //when
+
+        //then
+
+    }
+}
